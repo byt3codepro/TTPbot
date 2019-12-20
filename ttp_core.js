cea8683d8705417fe826982c5db25d4297aff2ca
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-20 23:26:48 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -2,15 +2,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    //client.user.setActivity('with a ČKD Tatra T6B5');
+    //client.user.setActivity('Russians', { type: 'LISTENING' });;
 });
 
 client.on('message', message => {
     if (message.content == 'ping?') {
-        if (message.author.id == <@179654608371712000>) {
-            message.react('<:verigood:641748105049407488>')
-    	    message.reply("🏓 *Pong!*");
-            }
+        //if (message.author.id == <@179654608371712000>) {
+        //    message.react('<:verigood:641748105049407488>')
+    	//    message.reply("🏓 *Pong!*");
+        //    }
         var a = ['no', 'leave me alone', 'noobs doesnt even have a ping', '**666 ms!** ***Burn in hell!***' 'GET LOST', 'im tired of you', 'idk', 'you get coal from me this christmas, only normal people get a ping present', 'This is a Premium feature! You are too poor to afford Premium of such cool bot!']
         message.react('⛔')
     	message.reply(a[Math.floor(Math.random() * a.length)]);
