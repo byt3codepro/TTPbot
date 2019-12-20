0e7d7443d466a4f3ab43062102a709b1fd810d3c
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-20 21:52:37 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -5,12 +5,9 @@ client.on('ready', () => {
     //client.user.setActivity('with a ČKD Tatra T6B5');
 });
 
-var today = new Date();
-let ttpTestChannel = client.channels.get("642490607079653416")
-ttpTestChannel.send(today)
-
 client.on('message', message => {
     if (message.content == 'ping?') {
+        message.react("🇧","🇷","🇺","🇭")
     	message.reply('oh fuck off its late and im tired');
   	}
 });
