cc18d2cc425a9f167b69550e6c104bda8aacaadf
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-20 21:30:04 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    //client.user.setActivity('with a ČKD Tatra T6B5');
+    client.user.setActivity('with a ČKD Tatra T6B5');
 });
 
 client.on('message', message => {
