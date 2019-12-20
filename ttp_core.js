e9d81e47f8dfc2925f8a98a142765f5078bbc22c
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-20 23:30:27 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    //client.user.setActivity('Russians', { type: 'LISTENING' });;
+    
 });
 
 client.on('message', message => {
