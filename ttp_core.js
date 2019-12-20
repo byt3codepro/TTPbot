9cf321b3839371bafb5cd5f226d1926f38966e7c
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-21 00:18:03 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -8,8 +8,8 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content == 'ping?') {
         if (message.author.id == 179654608371712000) {
-            message.react('<:verigood:641748105049407488>')
-    	    message.channel.send("🏓   ***Pong!***");
+            message.react('❤️')
+    	    message.channel.send("🏓\n***Pong!***");
             } else {        
                 var a = ['no', 'leave me alone', 'noobs doesnt even have a ping', '**666 ms!** ***Burn in hell!***', 'GET LOST', 'im tired of you', 'idk', 'you get coal from me this christmas, only normal people get a ping present', 'This is a Premium feature! You are too poor to afford Premium of such cool bot!']
                 message.react('⛔')
