9edba38bac5919a793c732e5fd3103e560225605
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-21 00:42:39 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -2,7 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setPresence({ game: { name: 'with discord.js' }, status: 'idle' })
+    client.user.setActivity('YouTube', { type: 'WATCHING' })
+    client.user.setStatus('idle')
 });
 
 client.on('message', message => {
