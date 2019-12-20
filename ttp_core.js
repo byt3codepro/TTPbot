420303b3a71795b035e557631f6fdd74914cac61
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-20 22:32:30 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -9,7 +9,7 @@ client.on('message', message => {
     if (message.content == 'ping?') {
         var a = ['no', 'leave me alone', 'GET LOST', 'im tired of you', 'idk', 'This is a Premium feature! You are too poor to afford Premium of such cool bot!']
         message.react('⛔')
-    	message.send(a[Math.floor(Math.random() * a.length)]);
+    	message.reply(a[Math.floor(Math.random() * a.length)]);
   	}
 });
 
