a5716400eb48a07c670bba5ad95da61bf9697194
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-20 23:40:02 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content == 'ping?') {
-        //if (message.author.id == <@179654608371712000>) {
-        //    message.react('<:verigood:641748105049407488>')
-    	//    message.reply("🏓 *Pong!*");
-        //    }
+        if (message.author.id == <@179654608371712000>) {
+            message.react('<:verigood:641748105049407488>')
+    	    message.reply("🏓 *Pong!*");
+            }
         var a = ['no', 'leave me alone', 'noobs doesnt even have a ping', '**666 ms!** ***Burn in hell!***', 'GET LOST', 'im tired of you', 'idk', 'you get coal from me this christmas, only normal people get a ping present', 'This is a Premium feature! You are too poor to afford Premium of such cool bot!']
         message.react('⛔')
     	message.reply(a[Math.floor(Math.random() * a.length)]);
