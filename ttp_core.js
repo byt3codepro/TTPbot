1a74a7166a23833fa52affd54de30ae4653781cd
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-21 01:18:59 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setGame("Game");
+    client.user.setStatus('dnd', 'NaN') 
 });
 
 client.on('message', message => {
