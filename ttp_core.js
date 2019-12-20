9cc8eee6bd282a4318d66dddd6479a19fbd2ba29
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-20 21:46:04 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -2,15 +2,16 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    var today = new Date();
-    let ttpTestChannel = client.channels.get("642490607079653416")
     //client.user.setActivity('with a ČKD Tatra T6B5');
-    ttpTestChannel.send(today)
 });
 
 client.on('message', message => {
     if (message.content == 'ping?') {
     	message.reply('oh fuck off its late and im tired');
+        
+        var today = new Date();
+        let ttpTestChannel = client.channels.get("642490607079653416")
+        ttpTestChannel.send(today)
   	}
 });
 
