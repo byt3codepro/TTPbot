632164b4d1ebf974b2ee2dc9dd388c1409e1f6e8
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-21 15:55:18 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -2,38 +2,43 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('with a ČKD Tatra T3SU', { type: 'PLAYING' }); //PLAYING / STREAMING / LISTENING / WATCHING
-    client.user.setStatus('online') //online - user is online / idle - user is AFK / offline - user is offline or invisible / dnd - user is in Do Not Disturb
+    client.user.setActivity('Mainentance, bot may not work', { type: 'PLAYING' }); //PLAYING / STREAMING / LISTENING / WATCHING
+    client.user.setStatus('dnd') //online - user is online / idle - user is AFK / offline - user is offline or invisible / dnd - user is in Do Not Disturb
 });
 
 client.on('message', message => {
-    if (message.content == 'ping?') {
-        if (message.author.id == 179654608371712000) {
-            message.react('❤️')
-    	    message.channel.send("🏓")
-            message.channel.send("*** Pong!***");
-            } else {        
-                var a = ['no', 'leave me alone', 'ask another bot im busy', '*Ping Service 1x* has been added to your cart\nNew Total: € 6.90', 'noobs doesnt even have a ping', '**666 ms!** ***Burn in hell!***', 'GET LOST', 'im tired of you', 'idk', 'you get coal from me this christmas, only normal people get a ping present', 'This is a Premium feature! You are too poor to afford Premium of such cool bot!']
-                message.react('⛔')
-    	        message.channel.send(a[Math.floor(Math.random() * a.length)]);
+    if (message.guild === null) {
+        message.channel.send("⛔   DM commands have been disabled!")
+    } else {
+        if (message.content == 'ping?') {
+            if (message.author.id == 179654608371712000) {
+                message.react('❤️')
+                message.channel.send("🏓")
+                message.channel.send("*** Pong!***");
+                } else {        
+                    var a = ['no', 'leave me alone', 'ask another bot im busy', '*Ping Service 1x* has been added to your cart\nNew Total: € 6.90', 'noobs doesnt even have a ping', '**666 ms!** ***Burn in hell!***', 'GET LOST', 'im tired of you', 'idk', 'you get coal from me this christmas, only normal people get a ping present', 'This is a Premium feature! You are too poor to afford Premium of such cool bot!']
+                    message.react('⛔')
+                    message.channel.send(a[Math.floor(Math.random() * a.length)]);
+                }
             }
+        if (message.content == '/cmds') {
+            message.channel.send('Due to budget cuts, this command has been disabled\nWe apologise for any inconvenience caused', {files: ['https://cdn.drawception.com/drawings/LrK4OWAvPQ.png']});
         }
-    if (message.content == '/cmds') {
-        message.channel.send('Due to budget cuts, this command has been disabled\nWe apologise for any inconvenience caused', {files: ['https://cdn.drawception.com/drawings/LrK4OWAvPQ.png']});
-    }
-    if (message.content == '/kevinreys') {
-        message.channel.send('Idiot with bad grammar, currently working in WMP Roblox as Superintendant for the most inactive unit in the force. \n     */ Wikipedia /*');
-    }
-    if (message.content == '/ralfotr') {
-        message.channel.send('Rightful owner of the well known TTP bot that provides real information across multiple Discord servers. Also known as a great developer in Roblox, specialising in development of public transport and urban infrastructure. \n     */ Wikipedia /*');
-    }
-    /*if (message.content == '/clear') {
-        if (message.member.hasPermission("ADMINISTRATOR")) {
-            message.channel.send('request passed');
-        } else {
-        message.channel.send('❗ Insufficient permissions');
+        if (message.content == '/kevinreys') {
+            message.channel.send('Idiot with bad grammar, currently working in WMP Roblox as Superintendant for the most inactive unit in the force. \n     */ Wikipedia /*');
         }
-    }*/
+        if (message.content == '/ralfotr') {
+            message.channel.send('Rightful owner of the well known TTP bot that provides real information across multiple Discord servers. Also known as a great developer in Roblox, specialising in development of public transport and urban infrastructure. \n     */ Wikipedia /*');
+        }
+        /*if (message.content == '/clear') {
+            if (message.member.hasPermission("ADMINISTRATOR")) {
+                message.channel.send('request passed');
+            } else {
+            message.channel.send('❗ Insufficient permissions');
+            }
+        }*/
+    }
+    
 
 });
 
