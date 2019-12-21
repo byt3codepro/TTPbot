5d5656fad815a1eb2dce9bd91a70deee38d9166b
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-21 17:26:17 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('with a ČKD Tatra T6B5', { type: 'PLAYING' }); //PLAYING / STREAMING / LISTENING / WATCHING
-    client.user.setStatus('online') //online - user is online / idle - user is AFK / offline - user is offline or invisible / dnd - user is in Do Not Disturb
+    client.user.setActivity('Mainantance, bot may not be available', { type: 'PLAYING' }); //PLAYING / STREAMING / LISTENING / WATCHING
+    client.user.setStatus('dnd') //online - user is online / idle - user is AFK / offline - user is offline or invisible / dnd - user is in Do Not Disturb
 });
 
 client.on('message', message => {
@@ -35,9 +35,9 @@ client.on('message', message => {
                 var delnum = message.content.split(" ");
                 message.channel.bulkDelete(parseInt(delnum[1]))
                 if (parseInt(delnum[1]) > 100) {
-                    message.reply("❗ You can only delete 100 or less messages at once").then(msg => {msg.delete(5000)}).catch
+                    message.reply("❗ You can only delete 100 or less messages at once").then(msg => {msg.delete(5000)}).catch()
                 } else {
-                    message.reply("Deleted **" + delnum[1] + "** messages!").then(msg => {msg.delete(5000)}).catch
+                    message.reply("Deleted **" + delnum[1] + "** messages!").then(msg => {msg.delete(5000)}).catch()
                 }
             } else {
                 message.channel.send('❗ Insufficient permissions');
