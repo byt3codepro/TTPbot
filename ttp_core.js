03b4ddcedf603bbc44e51089dda574cc306f5df1
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-21 16:31:08 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -33,7 +33,9 @@ client.on('message', message => {
         if (message.content.startsWith('/clear')) {
             if (message.member.hasPermission("ADMINISTRATOR")) {
                 message.channel.send('request passed');
-                
+                var delnum = message.content.split(" ",2);
+                message.channel.bulkdelete(parseInt(delnum[2]))
+)
             } else {
                 message.channel.send('❗ Insufficient permissions');
             }
