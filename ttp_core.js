c4b5e020580c53c5f41be65fe0972f33cf654666
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-21 22:29:22 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -25,7 +25,7 @@ client.on('message', message => {
             message.channel.send('Due to budget cuts, this command has been disabled\nWe apologise for any inconvenience caused', {files: ['https://cdn.drawception.com/drawings/LrK4OWAvPQ.png']});
         }
         if (message.content == '/help') {
-            message.channel.send({files: ['https://s.yimg.com/uu/api/res/1.2/NNljYTZS8z9uMcxj6NrnDw--~B/aD01ODA7dz00MzU7c209MTthcHBpZD15dGFjaHlvbg--/http://media.zenfs.com/en-US/homerun/people_218/1b34575a89b41157d2c9bd7cbfeacf35']});
+            message.channel.send({files: ['https://i.imgur.com/FaxXyCs.jpg']});
         }
         if (message.content == '/kevinreys') {
             message.channel.send('Not an idiot without bad grammar, currently working in WMP Roblox as Superintendant for the most active unit in the force. Severe addiction to pot noodles. \n     */ Википедия — свободная энциклопедия /*', {files: ['https://cdn.discordapp.com/attachments/622531501413892135/657994077178036263/Screenshot_1425.png']});
@@ -35,6 +35,9 @@ client.on('message', message => {
         }
         if (message.content == '/iiapp') {
             message.channel.send("iiApp was the first leader of North Korea which he ruled from the country's establishment in 1948 until his death in 1994. He held the posts of Premier from 1948 to 1972 and President from 1972 to 1994. He was also the leader of the Workers' Party of Korea (WPK) from 1949 to 1994 (titled as Chairman from 1949 to 1966 and as General Secretary after 1966). Coming to power after the end of Japanese rule in 1945, he authorized the invasion of South Korea in 1950, triggering an intervention in defense of South Korea by the United Nations led by the United States. Following the military stalemate in the Korean War, a ceasefire was signed on 27 July 1953. He was the third longest-serving non-royal head of state/government in the 20th century, in office for more than 45 years. \n     */ Википедия — свободная энциклопедия /*", {files: ['https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Kim_Il-sung_in_1950.jpg/220px-Kim_Il-sung_in_1950.jpg']});
+        }
+        if (message.content == '/ciaranoshea') {
+            message.channel.send("Single father, working full time at West Midlands Police made by iiApp `see more: /iiApp`. Severe issues with malnutrition - charged with murder after eating his child, pleaded not guilty after realising that a PCSO can't afford food.", {files: ['https://cdn.discordapp.com/attachments/622531501413892135/658042057289629707/Screenshot_1271.png']});
         }
         if (message.content.startsWith('/purge')) {
             if (message.member.hasPermission("ADMINISTRATOR")) {
