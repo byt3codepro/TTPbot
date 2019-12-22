91cf0c9f2efc62d29f3c0f971cf893ab7a16c061
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-22 16:42:04 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -55,7 +55,7 @@ client.on('message', message => {
                     if (parseInt(delnum[1]) > 100) {
                         message.reply("❗ You can only delete 100 or less messages at once").then(msg => {msg.delete(5000)})
                     } else {
-                        message.channel.bulkDelete(parseInt(delnum[1]))
+                        message.channel.bulkDelete(parseInt(delnum[1])).catch(error => console.log(error.stack));
                         message.reply("Deleted **" + delnum[1] + "** messages!").then(msg => {msg.delete(5000)})
                     }
                 } else {
