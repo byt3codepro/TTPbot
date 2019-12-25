9002bb046dc440d75999c463919fef2ddc553c7a
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2019-12-25 19:28:40 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -31,7 +31,15 @@ client.on('message', message => {
         if (message.content == '/help') {
             message.channel.send({files: ['https://i.imgur.com/FaxXyCs.jpg']});
         }
-        if (message.content.includes("fuck","FUCK","Fuck")) {
+        if (message.content.includes("fuck")) {
+            message.channel.send("❗ Uh oh " + message.author + "! That's a bad word! Next time use ***frick*** instead!")
+            message.delete();
+        }
+        if (message.content.includes("FUCK")) {
+            message.channel.send("❗ Uh oh " + message.author + "! That's a bad word! Next time use ***frick*** instead!")
+            message.delete();
+        }
+        if (message.content.includes("Fuck")) {
             message.channel.send("❗ Uh oh " + message.author + "! That's a bad word! Next time use ***frick*** instead!")
             message.delete();
         }
