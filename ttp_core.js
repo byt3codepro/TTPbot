00e462a07e81d243d7d6d7b84feae2bc5ffc3f73
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2020-01-01 23:49:01 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -21,8 +21,8 @@ client.on('message', message => {
     } else {
         if (message.content == '/results') {
             let ttpResultsChannel = client.channels.get("662034469708103690")
-            let results = [ttpResultsChannel.fetchMessage('662034597508546582')]
-            message.channel.send(results[1]);
+            let results = {ttpResultsChannel.fetchMessage('662034597508546582')}
+            message.channel.send(results);
         }
         
         
