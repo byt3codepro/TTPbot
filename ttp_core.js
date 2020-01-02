e8a1267818f8264eee5a5517004f9d6d38479819
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2020-01-02 22:58:49 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -65,8 +65,8 @@ client.on('message', message => {
 					.setDescription(mark)
 					.addField('User ID', userID)
 					.addField('Notes', "Applied for: " + role + "\n" + comment)
-					.setTimestamp()
 					.addField(specialRole)
+					.setTimestamp()
 					.setFooter('TRAMVAJU UN TROLEJBUSU PĀRVALDE', 'https://i.gyazo.com/c2da46134a992ace4d9ee3cda7cfe8b2.png');
 
 					message.author.send(resultsEmbed);
