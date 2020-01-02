6f37a3042dffac737db1001104638e1df94b387b
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2020-01-02 14:18:11 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -19,7 +19,7 @@ client.on('message', message => {
             let results = [ttpResultsChannel.fetchMessage('662034597508546582')]
             var resultSplit
             ttpResultsChannel.fetchMessage('662034597508546582').then(results)
-			resultSplit = results.content.split("\cut\")
+			resultSplit = results.content.split("{}")
             message.channel.send(resultSplit[1])
             
 																	  
