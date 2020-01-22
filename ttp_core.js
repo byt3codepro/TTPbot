8211e764be99778c15334731deb5558e99cd5189
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2020-01-22 16:01:31 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -5,7 +5,6 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     let ttpTestChannel = client.channels.get("642490607079653416")
-    let memez = client.channels.get("642490607079653416")
     ttpTestChannel.send("Bot restarted - " + Date())
     client.user.setActivity('with a Tatra T6B5SU', { type: 'PLAYING' }); //PLAYING / STREAMING / LISTENING / WATCHING         MAINTENANCE
     client.user.setStatus('online') //online - user is online / idle - user is AFK / offline - user is offline or invisible / dnd - user is in Do Not Disturb
@@ -15,6 +14,9 @@ client.on('message', message => {
     if (message.guild === null) {
         //space for dm commands
     } else {
+	    if (message.content == '/help') {
+            guild.createChannel('test', {parent: "🚨 » Support"})
+		}
        if (message.content == '/results') {
             let ttpResultsChannel = client.channels.get("662034469708103690")
 			var temp
@@ -75,47 +77,6 @@ client.on('message', message => {
 				}
         	})
         }
-        
-        
-        
-        if (message.content == '/kevinreys') {
-            message.channel.send('Marked: ***EPIC***. Not an idiot without bad grammar, but has parkinsons disease, currently working in WMP Roblox as CHIEF Superintendant for the most active unit in the force and owns the best force in Roblox - brit bus cops (BBC). Severe addiction to pot noodles. \n     */ Wikipedia - The Free Encyclopedia /*', {files: ['https://cdn.discordapp.com/attachments/622531501413892135/657994077178036263/Screenshot_1425.png']});
-        }
-        if (message.content == '/ralfotr') {
-            message.channel.send('Rightful owner of the well known TTP bot that provides real information across multiple Discord servers. Also known as a great developer in Roblox, specialising in development of public transport and urban infrastructure. \n     */ Wikipedia - The Free Encyclopedia /*', {files: ['https://image.prntscr.com/image/XIN6YMgfSZS1C8m9X6O7CQ.png']});
-        }
-        if (message.content == '/iiapp') {
-            message.channel.send("iiApp was the first leader of North Korea which he ruled from the country's establishment in 1948 until his death in 1994. He held the posts of Premier from 1948 to 1972 and President from 1972 to 1994. He was also the leader of the Workers' Party of Korea (WPK) from 1949 to 1994 (titled as Chairman from 1949 to 1966 and as General Secretary after 1966). Coming to power after the end of Japanese rule in 1945, he authorized the invasion of South Korea in 1950, triggering an intervention in defense of South Korea by the United Nations led by the United States. Following the military stalemate in the Korean War, a ceasefire was signed on 27 July 1953. He was the third longest-serving non-royal head of state/government in the 20th century, in office for more than 45 years. \n     */ Wikipedia - The Free Encyclopedia /*", {files: ['https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Kim_Il-sung_in_1950.jpg/220px-Kim_Il-sung_in_1950.jpg']});
-        }
-        if (message.content == '/ciaranoshea') {
-            message.channel.send("Single father, working full time at West Midlands Police made by iiApp `see more: /iiApp`. Severe issues with malnutrition - charged with murder after eating his child, pleaded not guilty after realising that a PCSO can't afford food. \n     */ Wikipedia - The Free Encyclopedia /*", {files: ['https://cdn.discordapp.com/attachments/622531501413892135/658042057289629707/Screenshot_1271.png']});
-        }
-        if (message.content == '/brianmcaskill') {
-            message.channel.send("Running the most inactive and useless unit in West Midlands Police - OSU. Got the job after threats from iiApp `see more: /iiApp` who knew that Mr. McCaskill was exploiting underage children at his 'StateFarm' credit and insurance company. He has also been accused of 3x Credit Fraud, Threats to a goverment official and 19x Urinating in public (not guilty due to bladder management issues). \n     */ Wikipedia - The Free Encyclopedia /*", {files: ['https://pbs.twimg.com/media/C9OhQiGXoAQT1aM.jpg']});
-        }
-        if (message.content == '/thomasbaker') {
-            if (message.author.id == 179654608371712000) {
-                message.channel.send("The hot handsome man that everyone keeps talking about but has never seen. Spends free time participating in mainly policing roleplay but also can't resist some furry and *Roblox Rule 34* roleplay in Roblox. Roleplaying in West Midlands Police Thomas has been interacting to undarge children like the developer of this bot, who was left frightened after seeing good grammar on his DMs. \n     */ Wikipedia - The Free Encyclopedia /*", {files: ['https://i.gyazo.com/66dbb90831844d2433fb804bf1d4df7f.jpg']});
-            }
-        }
-        if (message.content == '/cc') {
-            if (message.author.id == 179654608371712000) {
-                message.member.addRole('658414596712955906')
-            }
-        }
-	if (message.content == '/dcc') {
-            if (message.author.id == 179654608371712000) {
-                message.member.addRole('658414597891686404')
-            }
-        }
-	if (message.content == '/admin') {
-            if (message.author.id == 179654608371712000) {
-                message.member.addRole('661997745019682818')
-            }
-        }
-        
-        
-        
         if (message.content == 'ping?') {
             if (message.author.id == 179654608371712000) {
                 message.react('❤️')
@@ -129,9 +90,6 @@ client.on('message', message => {
             }
         if (message.content == '/cmds') {
             message.channel.send('Due to budget cuts, this command has been disabled\nWe apologise for any inconvenience caused', {files: ['https://cdn.drawception.com/drawings/LrK4OWAvPQ.png']});
-        }
-        if (message.content == '/help') {
-            message.channel.send({files: ['https://i.imgur.com/FaxXyCs.jpg']});
         }
         if (message.content.startsWith('/ttp')) {
             var lang = message.content.split(" ");
