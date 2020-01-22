24aa454a889015f91c93d71671dbb6f8e3d8f7f2
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2020-01-22 17:11:35 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -16,10 +16,10 @@ client.on('message', message => {
     } else {
 	    if (message.content.startsWith('/help')) {
 			if (message.content != '/help') {
-				var reason = message.content.split(" ", 1);
+				var reason = message.content.split(" ");
 				message.guild.createChannel('test', {type: 'text'}).then(newchannel => {
 					newchannel.setParent('669536429189365761')
-					newchannel.send("**Help request opened - " + Date() + "**\nAuthor: " + message.author + "\n" + reason[1] + "\n<@&669535649409269770>");
+					newchannel.send("**Help request opened - " + Date() + "**\nAuthor: " + message.author + "\n```" + reason[1] + "```\n<@&669535649409269770>");
 				})
 			} else {
 				message.channel.send('❗ Enter valid question or reason for requesting help   ``/help [question/reason]``');
