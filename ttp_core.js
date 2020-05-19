cc3d83f7dc0437b6a3e84870366c954250e2004d
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2020-05-20 00:17:58 +0300

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -8,8 +8,8 @@ client.on('ready', () => {
     //ttpTestChannel.send("Bot restarted - " + Date())
 	//let bitch = client.channels.get("685936439925407753")
     //bitch.send("***slayerzplayerz mom smells 😹 @everyone ***") 
-    //client.user.setActivity('PowerPoint', { type: 'PLAYING' }); //PLAYING / STREAMING / LISTENING / WATCHING         MAINTENANCE
-    client.user.setStatus('online') //online - user is online / idle - user is AFK / offline - user is offline or invisible / dnd - user is in Do Not Disturb
+    client.user.setActivity('MAINTENANCE', { type: 'PLAYING' }); //PLAYING / STREAMING / LISTENING / WATCHING         MAINTENANCE
+    client.user.setStatus('dnd') //online - user is online / idle - user is AFK / offline - user is offline or invisible / dnd - user is in Do Not Disturb
 });
 
 client.on('message', message => {
