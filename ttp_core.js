4d38bf6288820ba52202aae250f1c1e3c1aaedbd
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2020-10-23 12:59:52 +0300

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -82,8 +82,13 @@ var whitelist = ["179654608371712000", "514127283636797450", "746662409724231798
             }   
         }
 	if (message.content == '/lv') {
-		message.member.addRole("705757268448378971")
-		message.react('✅')
+		if (message.author.id == "602742940074246144") {
+			message.channel.send('❗ Root Issue (7)\n```Root exited with false member value!\nLUG673432\n\n/lvYES.ID705757268448378971.X.REACT=X```');
+			message.react('❌')
+		} else {
+			message.member.addRole("705757268448378971")
+			message.react('✅')
+		}
 	}
 	if (message.content == '/lv remove') {
 		message.member.removeRole("705757268448378971")
