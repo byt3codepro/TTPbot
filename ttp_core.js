2bac5c471af7c185d544b58f533bd6b770990f86
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2021-01-09 02:32:06 +0200

    Update ttp_core.js

--- ttp_core.js
+++ ttp_core.js
@@ -22,7 +22,7 @@ var whitelist = ["179654608371712000", "514127283636797450", "746662409724231798
 	botdms.send(message.content)
 	const exampleEmbed = new Discord.MessageEmbed()
 	.setColor('#2dcc70')
-	.setTitle(message.author)
+	.setTitle(message.author.tag + "  |  " + message.author)
 	.setDescription(message.content)
 	botdms.send(exampleEmbed);
 	    
