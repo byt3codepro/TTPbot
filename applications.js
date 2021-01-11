699a21f01637d4dc0904b72f5d20b7a73ac7c71a
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2021-01-11 20:02:28 +0200

    Update applications.js

--- applications.js
+++ applications.js
@@ -7,9 +7,9 @@ const client = new Discord.Client();
 client.on('message', message => {
 		//   space for dm commands  
 		
-		if (message.content == "?testing") {
-			message.reply("Tested!")
-		}
+		//if (message.content == "?testing") {
+		//	message.reply("Tested!")
+		//}
 		
 		//   space for dm commands  
     //} else {
