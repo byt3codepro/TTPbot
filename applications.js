0e3eb651f708cb91183db44d0d48d78389aabc8d
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2021-01-13 00:45:10 +0200

    Update applications.js

--- applications.js
+++ applications.js
@@ -19,7 +19,7 @@ async function results() {
 	});
 	await doc.loadInfo(); // loads document properties and worksheets
 	const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
-	for (let i = 1; i < array.length; i++) {
+	for (let i = 1; i < 1000; i++) {
 		if (sheet.getCellByA1('B' + i).value == "FALSE") {
 			if (sheet.getCellByA1('W' + i).value == "") {
 				break;
