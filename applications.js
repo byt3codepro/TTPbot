6ae94574f87f12449066e5cfd0665b28f22a7d63
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2021-01-13 00:26:14 +0200

    Update applications.js

--- applications.js
+++ applications.js
@@ -14,20 +14,19 @@ client.on('message', message => {
 async function reults() {
 	// Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
 	await doc.useServiceAccountAuth({
-	  client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-	  private_key: process.env.GOOGLE_PRIVATE_KEY,
+		client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+		private_key: process.env.GOOGLE_PRIVATE_KEY,
 	});
 	await doc.loadInfo(); // loads document properties and worksheets
 	const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
-    
-    for (let i = 1; i < array.length; i++) {
-        if (sheet.getCellByA1('B' + i) == "FALSE") {
-            const mark = sheet.getCellByA1('A' + i);
-            message.channel.send(mark.value)
-        } else {
-            message.channel.send("Already sent!");
-        }
-    }
+	for (let i = 1; i < array.length; i++) {
+		if (sheet.getCellByA1('B' + i) == "FALSE") {
+			const mark = sheet.getCellByA1('A' + i);
+			message.channel.send(mark.value)
+		} else {
+			message.channel.send("Already sent!");
+		}
+	}
 }
 
 client.login(process.env.BOT_TOKEN);
