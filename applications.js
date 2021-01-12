246f50e9750eeda0106ba7156d43e0fffc259f67
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2021-01-13 00:10:04 +0200

    Update applications.js

--- applications.js
+++ applications.js
@@ -1,38 +1,34 @@
 //©raltec 2021
 const { GoogleSpreadsheet } = require('google-spreadsheet');
-
-// Initialize the sheet - doc ID is the long id in the sheets URL
 const doc = new GoogleSpreadsheet('1dDs1zvYx4KUEwB1B9qRSsana0rRLw1UsPsaXUl7PF3g');
-
-async function padla() {
-	// Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
-	await doc.useServiceAccountAuth({
-	  client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-	  private_key: process.env.GOOGLE_PRIVATE_KEY,
-	});
-
-	await doc.loadInfo(); // loads document properties and worksheets
-	console.log(doc.title);
-	await doc.updateProperties({ title: 'renamed doc' });
-
-	const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
-	console.log(sheet.title);
-	console.log(sheet.rowCount);
-}
-	
-	
-	
-	
-	
 	
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('message', message => {
-		if (message.content == "?testing") {
-			padla();
+		if (message.content == "/results") {
+			results();
 		}
 });
+
+async function reults() {
+	// Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
+	await doc.useServiceAccountAuth({
+	  client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+	  private_key: process.env.GOOGLE_PRIVATE_KEY,
+	});
+	await doc.loadInfo(); // loads document properties and worksheets
+	const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+    
+    for (let i = 1; i < array.length; i++) {
+        if (sheet.getCellByA1('B' + i) == "FALSE") {
+            const mark = sheet.getCellByA1('A' + i);
+            message.channel.send(mark.value)
+        } else {
+            message.channel.send("Already sent!");
+        }
+    }
+}
 
 client.login(process.env.BOT_TOKEN);
 //©raltec 2021
