03e8d7249c7747c88786926aa387c9d4a5eb2e34
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2021-01-13 17:48:11 +0200

    Update applications.js

--- applications.js
+++ applications.js
@@ -36,7 +36,6 @@ async function results(message) {
 		const tag = sheet.getCellByA1('M' + i);
 		const rank = sheet.getCellByA1('P' + i);
     		const applicant = guild.members.cache.find((padla) => padla.name == tag.value)
-		console.log(applicant)
 		if (mark.value === null) {
 			break;
 		} else {
@@ -132,7 +131,7 @@ async function results(message) {
 					}
 				}
 				sent.value = "☑"
-				console.log(applicant)
+				console.log(guild.members.cache.find((member) => member.name == tag.value))
 				//applicantid.send(resultsembed).catch(err => message.reply(err + "     ``USER:(" + tag.value + ")``"));
 				await sheet.saveUpdatedCells();
 			} else {
