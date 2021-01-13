a85f24122d38f4b9fc9b2e137cb289a219bffb8b
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2021-01-13 13:56:03 +0200

    Update applications.js

--- applications.js
+++ applications.js
@@ -37,7 +37,7 @@ async function results(message) {
 			break;
 		} else {
 			if (sent.value === "☐") {
-				if (comments === null) {
+				if (comments.value === null) {
 					if (mark.value === "PASSED") {
 						resultsembed = new Discord.MessageEmbed()
 						.setColor('#2dcc70') //RED - #E74C3C | ORANGE - #CA6F1E | LUGANE GREEN - #2DCC70
