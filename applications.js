ac37add49ecf740b89443cba6e8738da5052ebe6
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2021-01-23 00:58:04 +0200

    Update applications.js

--- applications.js
+++ applications.js
@@ -273,8 +273,8 @@ async function issuefine(message) {
 		{ name: 'Pārkāpums fiksēts', value: date + ", " + time + ", " + location, inline: false }
 		)
 		const guild = await client.guilds.fetch('705686666043457606')
-		speeder = guild.members.cache.find((member) => member.displayName == username)
-		guild.members.cache.find((member) => member.displayName == username).send(fineEmbed).catch(err => {
+		speeder = guild.members.cache.find((member) => member.displayName == username).catch(err => {console.log(err));
+		speeder.send(fineEmbed).catch(err => {
 			if (err === "DiscordAPIError: 404: Not Found") {
 				message.react('❌')
 				message.channel.send("❗ Something went wrong! Refer to the error log below.\n\n``User with this ID hasn't been found! Error:\n" + err + '``')
