82bcf647f2565feeea7e6c20b0d54e5dcaddf8cd
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2021-02-20 14:06:08 +0200

    Update applications.js

--- applications.js
+++ applications.js
@@ -351,8 +351,8 @@ setInterval(async function reminderCheck() {
 		} else {
 			const split = Time.split(":");
 			if (split[0] == hr && split[1] == min) {
-				if (Perm != "x") {
-					await DelReq = "x"
+				if (Perm == null) {
+					DelReq = "x"
 				}
 				await sheet.saveUpdatedCells();
 				if (Type == "dm") {
