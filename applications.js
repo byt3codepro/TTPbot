558b53b521e9662a3081ee7d7f87f6909417fd96
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2022-03-06 13:00:32 +0200

    f

--- applications.js
+++ applications.js
@@ -509,7 +509,7 @@ async function replydm(message) {
 			if (senderuser === undefined || senderuser === null) {
 				message.reply("❗ Original message cannot be retrived (most probably deleted by the sender)1!")
 			} else {
-				const createddm = await senderuser.createDM().fetch()
+				const createddm = await senderuser.createDM()
 				if (createddm === undefined) {
 					message.reply("❗ Original message cannot be retrived (most probably deleted by the sender)2!")
 					console.log(createddm)
@@ -519,7 +519,7 @@ async function replydm(message) {
 				} else {
 					//originalmessage = await createddm.messages.cache.get(msgid)
 					console.log(senderuser)
-					console.log(createddm)
+					console.log(createddm.fetch())
 					console.log(originalmessage)
 				}
 			}
