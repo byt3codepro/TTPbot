64e9c260db36501d6d366f51c404f1aafe678f59
Ralfotr <59094587+Ralfotr@users.noreply.github.com>
2022-04-07 17:14:48 +0300

    f

--- applications.js
+++ applications.js
@@ -136,7 +136,7 @@ async function results(message) {
 			const rank = sheet.getCellByA1('P' + i);
 			try {
 				//applicant = client.users.cache.get(tag.value)
-				applicant = client.users.cache.get(tag.value); //client.users.cache.find(member => member.user.id == tag.value) //guild.members.cache.find(member => member.user.tag == tag.value)
+				applicant = client.guilds.cache.get("705686666043457606").members.cache.get(tag.value) //client.users.cache.find(member => member.user.id == tag.value) //guild.members.cache.find(member => member.user.tag == tag.value)
 			} catch(err) {
 				console.error(err)
 			}
